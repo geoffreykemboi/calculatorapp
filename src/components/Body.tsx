4d5862d439a6d7b052c3b4450294cbae329baec6
geoffreykemboi <geoffreyk126@gmail.com>
2025-04-26 17:30:40 +0300

    change

---
+++ src/components/Body.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Body() {
+  return (
+    <div className="body-container">
+      {/* Contacts Section */}
+      <section className="contacts">
+        <h2>Contact Us</h2>
+        <p>Email: geoffreyk126@gmail.com</p>
+        <p>Phone: +254 0726328050</p>
+        <p>Address: 123 Brookside Road Westlands</p>
+      </section>
+
+
+    </div>
+  );
+}
+
+export default Body;
+
+
+
+
